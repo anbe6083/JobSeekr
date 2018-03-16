806d2cdfa4a7895a5e61e8e68e425b341c4b78b8
anbe6083 <anbe6083@colorado.edu>
2018-03-16 16:30:56 -0700

    Added /api/authJobs route for static request to authenticJobs

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
 const express = require('express');
-
 const app = express();
+const axios = require('axios');
 
 app.get('/', (req, res) => {
   res.send({ test: 'hello world' });
+});
+
+const authJobsURL =
+  'https://authenticjobs.com/api/?api_key=38f3b1b69046ec1828bddd633ccc8fb4&method=aj.jobs.search&keywords=Software%20Engineer, Front%20End%20Engineer&format=json&telecommuting=1';
+
+app.get('/api/authJobs', (req, res) => {
+  axios.get(authJobsURL).then(json => {
+    res.send(json.data.listings['listing']);
+  });
 });
 
 const PORT = process.env.PORT || 5000;
