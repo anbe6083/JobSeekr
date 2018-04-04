108d1c532c788831e99f26b8811b7b4591e5d174
anbe6083 <anbe6083@colorado.edu>
2018-04-03 18:30:10 -0700

    Added sidenav

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,34 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import Wizard from './components/Wizard';
+// import WizardForm from './components/src/WizardForm';
+import { SideNav, SideNavItem, Button } from 'react-materialize';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Wizard />
+        {/* <WizardForm /> */}
+        <SideNav
+          trigger={<Button>SIDE NAV DEMO</Button>}
+          options={{ closeOnClick: true }}
+        >
+          <SideNavItem
+            userView
+            user={{
+              background: 'img/office.jpg',
+              image: 'img/yuna.jpg',
+              name: 'John Doe',
+              email: 'jdandturk@gmail.com'
+            }}
+          />
+          <SideNavItem href="#!icon" icon="cloud">
+            Dashboard
+          </SideNavItem>
+          <SideNavItem href="#!second">Companies I'm Interested In</SideNavItem>
+          <SideNavItem href="#!second">People I've Networked With</SideNavItem>
+          <SideNavItem href="#!third">Application Statuses</SideNavItem>
+        </SideNav>
       </div>
     );
   }
--- client/src/components/states.js
+++
@@ -1,8 +0,0 @@
-import keyMirror from 'keymirror';
-
-export const states = keyMirror({
-  LOCATION: true,
-  POSITION: true,
-  PERKS: true,
-  REMOTE: true
-});
