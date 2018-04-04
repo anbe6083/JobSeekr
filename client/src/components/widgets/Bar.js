6303addd2a934bf297196fa1d613f3b07c72aff7
anbe6083 <anbe6083@colorado.edu>
2018-04-03 20:00:10 -0700

    Changed hover colors

--- client/src/components/widgets/Bar.js
+++ client/src/components/widgets/Bar.js
@@ -21,17 +21,17 @@ export default class BarChart extends Component {
             backgroundColor: '#006BA6',
             borderColor: '#006BA6',
             borderWidth: 1,
-            hoverBackgroundColor: '#006BA6',
-            hoverBorderColor: '#006BA6',
+            hoverBackgroundColor: '#004266',
+            hoverBorderColor: '#004266',
             data: [1, 2, 3, 4, 5, 6, 7]
           },
           {
             label: 'Rejected',
-            backgroundColor: 'rgba(255,99,132,0.2)',
-            borderColor: 'rgba(255,99,132,1)',
+            backgroundColor: '#7B2D26',
+            borderColor: '#7B2D26',
             borderWidth: 1,
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            hoverBorderColor: 'rgba(255,99,132,1)',
+            hoverBackgroundColor: '#4C1C17',
+            hoverBorderColor: '#4C1C17',
             data: [10, 20, 30, 40, 50, 60, 70]
           },
           {
@@ -39,8 +39,8 @@ export default class BarChart extends Component {
             backgroundColor: '#23CE6B',
             borderColor: '#23CE6B',
             borderWidth: 1,
-            hoverBackgroundColor: '#23CE6B',
-            hoverBorderColor: '#23CE6B',
+            hoverBackgroundColor: '#1BA354',
+            hoverBorderColor: '#1BA354',
             data: [3, 5, 7, 9, 11, 13, 15]
           },
           {
@@ -49,8 +49,8 @@ export default class BarChart extends Component {
             borderColor: '#F2BB05',
             borderWidth: 1,
             borderColor: '#F2BB05',
-            hoverBackgroundColor: 'rgba(255,99,132,0.4)',
-            borderColor: '#F2BB05',
+            hoverBackgroundColor: '#C99B04',
+            borderColor: '#C99B04',
             hoverBorderColor: 'rgba(255,99,132,1)',
             data: [2, 4, 6, 8, 10, 12, 14]
           }
