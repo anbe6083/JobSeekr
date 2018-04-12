9b26cae44d3252ea5459e7fb3a79ed6db78e66e8
anbe6083 <anbe6083@colorado.edu>
2018-04-12 10:42:14 -0700

    Added authRoutes reducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -2,10 +2,12 @@ import { combineReducers } from 'redux';
 import networkReducers from './networkReducers';
 import applicationReducers from './applicationReducers';
 import dashboardReducers from './dashboardReducers';
+import authReducer from './authReducer';
 const rootReducer = combineReducers({
   myNetwork: networkReducers,
   myApplications: applicationReducers,
-  dashboardReducers: dashboardReducers
+  dashboardReducers: dashboardReducers,
+  authReducer: authReducer
 });
 
 export default rootReducer;
